10b0db2de3258a4d1c236760af71e2a286af4bfa
Somabrata Mazumdar <sommazum@cisco.com>
2024-11-29 19:58:54 +0530

    fix react stockchart module issue

--- src/app/components/stocks-chart.tsx
+++ src/app/components/stocks-chart.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CanvasJSReact from '@canvasjs/react-stockcharts';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
